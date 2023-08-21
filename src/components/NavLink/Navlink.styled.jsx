2fdeac97ca4db94b54c60977f58f6e53540b4836
Soundlover1984 <denys.kovtun@gmail.com>
2023-08-21 20:10:13 +0300

    Update Navlink.styled.jsx

--- src/components/NavLink/Navlink.styled.jsx
+++ src/components/NavLink/Navlink.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const Link = styled(NavLink)`
-  padding: 8px 16px;
+  padding: 8px;
   display: inline-flex;
   align-items: center;
   justify-content: center;
@@ -10,15 +10,20 @@ export const Link = styled(NavLink)`
   text-decoration: none;
   color: black;
   font-weight: bold;
+  font-size: 16px;
   transform: scale(1);
   box-shadow: none;
+  height: fit-content;
+  border: 1px solid transparent;
+
   &:hover,
   &:focus {
-    transform: scale(1.1);
+    /* transform: scale(1.1); */
     border: ${props =>
       props.status === 'login' || props.status === 'register'
-        ? 'none'
+        ? '1px solid transparent'
         : ' 1px solid rgba(0, 128, 75, 0.7)'};
+
     background-color: ${props =>
       props.status === 'add'
         ? 'rgba(0, 128, 75, 0.7)'
@@ -27,18 +32,31 @@ export const Link = styled(NavLink)`
         : props.status === 'register'
         ? 'rgba(248, 220, 92, 0.7)'
         : 'transparent'};
+
     box-shadow: ${props =>
       props.status === 'login' || props.status === 'register'
         ? '0px 14px 6px 4px #cab1b98c'
         : '0px 12px 8px 4px #0b78474d'};
   }
+
   &.active {
     color: white;
     background-color: rgba(0, 128, 75, 0.7);
+
     &:hover,
     &:focus {
       border: none;
       color: inherit;
     }
+  }
+
+  @media screen and (min-width: 320px) {
+  }
+  @media screen and (min-width: 768px) {
+    padding: 8px 16px;
+    font-size: 20px;
+  }
+
+  @media screen and (min-width: 1280px) {
   }
 `;
