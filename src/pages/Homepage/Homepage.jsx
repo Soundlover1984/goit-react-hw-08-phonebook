c3d56819d53636d3c9dfe9e70bd240c6f3df4ad3
Soundlover1984 <denys.kovtun@gmail.com>
2023-08-21 22:01:07 +0300

    Homepage finished

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,22 +1,58 @@
 import { useSelector } from 'react-redux';
+import { FaRegCopyright } from 'react-icons/fa';
 
-import { HomeMain, HomeTitle } from './Homepage.styled';
+import {
+  HomeMain,
+  ImgWrapperUnlogged,
+  ImgWrapper,
+  HomeTitle,
+  Container,
+  ContainerUnlogged,
+  HomeTitleUnlogged,
+  Footer,
+  Copyright,
+  CopyrightText,
+} from './Homepage.styled';
 import { selectLogStatus, selectUserName } from 'redux/selectors';
-
+import Animation from '../../images/people.gif';
+import Animation2 from '../../images/app.gif';
+import { PhonebookLogo } from 'components/Logo/Logo';
 const Homepage = () => {
   const isLoggedIn = useSelector(selectLogStatus);
   const name = useSelector(selectUserName);
   return (
     <HomeMain>
       {isLoggedIn ? (
-        <>
-          <HomeTitle>Welcome to your Phonebook, {name}</HomeTitle>
-        </>
+        <Container>
+          <ImgWrapper>
+            <img alt="animation" width="100%" srcSet={Animation} />
+          </ImgWrapper>
+          <HomeTitle>
+            Hey, <span>{name}</span>, let's check your <PhonebookLogo /> !{' '}
+            <br />
+            Here we go!
+          </HomeTitle>
+        </Container>
       ) : (
-        <>
-          <HomeTitle>Welcome to Phonebook!</HomeTitle>
-        </>
+        <ContainerUnlogged>
+          <ImgWrapperUnlogged>
+            <img alt="animation" width="100%" srcSet={Animation2} />
+          </ImgWrapperUnlogged>
+          <HomeTitleUnlogged>
+            Welcome to <PhonebookLogo /> ! <br />
+            Keep your contacts well organized!
+          </HomeTitleUnlogged>
+        </ContainerUnlogged>
       )}
+      <Footer>
+        <Copyright>
+          <CopyrightText>
+            Copyright
+            <FaRegCopyright size={16} />
+            2023 <br /> All rights reserved
+          </CopyrightText>
+        </Copyright>
+      </Footer>
     </HomeMain>
   );
 };
--- src/pages/SharedLayout/SharedLayout.jsx
+++ src/pages/SharedLayout/SharedLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Suspense } from 'react';
+
 import { Container, Header } from './SharedLayout.styled';
 import { Loader } from '../../components/Loader/Loader';
 import { Navigation } from 'components/Navigation/Navigation';
 import { UserLogoutMenu } from 'components/UserLogoutMenu/UserLogoutMenu';
 import { UserLoginMenu } from 'components/UserLoginMenu/UserLoginMenu';
 import { selectLogStatus } from 'redux/selectors';
+import { PhonebookLogo } from 'components/Logo/Logo';
 
 export const SharedLayout = () => {
   const isLoggedIn = useSelector(selectLogStatus);
@@ -14,6 +16,7 @@ export const SharedLayout = () => {
     <Container>
       <Header>
         <Navigation />
+        <PhonebookLogo />
         {isLoggedIn ? <UserLogoutMenu /> : <UserLoginMenu />}
       </Header>
       <Suspense fallback={<Loader />}>
