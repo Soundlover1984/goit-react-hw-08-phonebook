5461c0a4f8f8e8a406ea3028804757e7c7d59c3c
Soundlover1984 <denys.kovtun@gmail.com>
2023-08-21 21:11:36 +0300

    Update NavLink.jsx

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -1,4 +1,4 @@
-import { Link } from './/NavLink.styled';
+import { Link } from './NavLink.styled';
 
 export const NavLink = ({ to, status, text }) => {
   return <Link  status={status} to={to}>{text}</Link>;
