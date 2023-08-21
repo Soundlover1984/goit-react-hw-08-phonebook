a78141c176d2e9a1f589833cd16601b8853ed681
Soundlover1984 <denys.kovtun@gmail.com>
2023-08-21 22:28:58 +0300

    contacts refactored and exported

--- src/pages/Contacts/Contacts.styled.jsx
+++ src/pages/Contacts/Contacts.styled.jsx
@@ -1,23 +1,43 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.main`
+  padding: 0 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  flex-direction: column;
+  color: #010101;
+
+  @media screen and (min-width: 768px) {
+    padding: 0 42px;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding: 0 62px;
+  }
+`;
+
+export const BlockWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  font-size: 40px;
-  color: #010101;
-  padding: 40px;
-`;
-
-export const MainHeader = styled.h1`
-  font-size: 42px;
-  color: #010101;
-  text-transform: uppercase;
 `;
 
 export const SubHeader = styled.h2`
-  font-size: 36px;
-  color: #010101;
+  font-size: 16px;
   text-transform: uppercase;
+  margin-top: 0;
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
+  @media screen and (min-width: 1280px) {
+    font-size: 28px;
+  }
 `;
