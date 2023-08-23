b23cb35f0c5eba5189a7c5a764a081f11fb7e2e7
Denys Kovtun <denys.kovtun@gmail.com>
2023-08-23 23:49:37 +0300

    Update notifySettings.jsx

--- src/utils/notifySettings.jsx
+++ src/utils/notifySettings.jsx
@@ -2,7 +2,7 @@ export const notifySettings = {
     width: '380px',
     position: 'right-top',
     distance: '10px',
-    opacity: 1,
+    opacity: 0.7,
     fontSize: '20px',
     borderRadius: '12px',
   };
